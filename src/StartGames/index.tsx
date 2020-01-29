2f7ffd6f8a41a8f26c3ef87accfb8afd51a3b609
ino_h <syslink.h.inoue@gmail.com>
2020-01-29 13:35:47 +0900

    amplify の Storage に対応

--- src/StartGames/index.tsx
+++ src/StartGames/index.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 import useFetch from 'use-http';
 import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { Storage } from 'aws-amplify';
 
 import Form from 'components/Form';
 import { SubmitButton } from 'components/Button';
+
+Storage.configure({
+  AWSS3: {
+      bucket: 'amplify-serviceweb-dev-222920-deployment',//Your bucket name;
+      region: 'ap-northeast-1'//Specify the region your bucket was created in;
+  }
+});
 
 const StartGame: React.FC = () => {
   const [request] = useFetch('http://localhost:8000');
@@ -17,10 +25,20 @@ const StartGame: React.FC = () => {
       .then(response => history.push(`/tables/${response.id}`));
   };
 
+  const upload = () => {
+    Storage.put('test.txt', 'Hello')
+      .then (result => console.log(result))
+      .catch(err => console.log(err));
+  };
+
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
-      <SubmitButton value="ゲーム開始" />
-    </Form>
+    <>
+      <button onClick={upload}>upload</button>
+
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <SubmitButton value="ゲーム開始" />
+      </Form>
+    </>
   );
 }
 
